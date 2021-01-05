ee01d3d14e94100b8f338296833605bbe695fd31
Jardel <jardel1101@gmail.com>
2021-01-05 12:28:28 -0300

    Backend project created

---
+++ backend/src/test/java/com/jbdev/fooddeliver/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbdev.fooddeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
